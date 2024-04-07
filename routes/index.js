17e3166e2f7d84b62e770503c8f4f0b28e581ebf
taufikmaulana405 <taufikmaulana405@gmail.com>
2024-04-08 00:29:08 +0700

    Menambahkan Route root directory untuk dialihkan ke swagger

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,10 @@ const router = express.Router();
 router.use("/crud_mahasiswa", require("./crud_mahasiswa"));
 router.use("/docs", require("./docs"));
 
+// Home root dialihkan to /docs untuk menampilkan dokumentasi
+router.get("/", (req, res) => {
+    res.redirect("/docs");
+});
+
 // Export Module
 module.exports = router;
